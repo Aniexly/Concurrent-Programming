f7d62e07315f242e91f0b896ee669f0f48bebc27
Agata Gontarek <gontarekagata200509@gmail.com>
2026-05-15 22:03:33 +0200

    Fixed ball collision

--- Logic/LogicAPI.cs
+++ Logic/LogicAPI.cs
@@ -62,7 +62,8 @@ namespace Logic
             await Task.Run(() =>
             {
                 HandleCollisionWithBall(ball, board);
-                HandleCollisionWithWall(ball, board);
+                HandleCollisionWithWallAndMove(ball, board);
+                MakeSureThatBallIsInsideBoard(ball, board);
             });
         }
 
@@ -73,7 +74,7 @@ namespace Logic
             {
                 try
                 {
-                    if (DoesCollideWithBall(ball, otherBall))
+                    if (DoBallsCollide(ball, otherBall))
                     {
                         BounceBalls(ball, otherBall);
                     }
@@ -83,8 +84,6 @@ namespace Logic
                     Monitor.Exit(otherBall);
                 }
             }
-            ball.Position.X += ball.Velocity.X;
-            ball.Position.Y += ball.Velocity.Y;
         }
 
         private List<IBall> FindPossiblyCollidingBalls(IBall ball, IBoard board)
@@ -106,47 +105,74 @@ namespace Logic
             return doesCollide;
         }
 
-        public double CalculateDistance(IPosition position1, IPosition position2)
+        private double CalculateDistance(IPosition position1, IPosition position2)
         {
             double dx = position1.X - position2.X;
             double dy = position1.Y - position2.Y;
             return Math.Sqrt(dx * dx + dy * dy);
         }
 
-        private bool DoesCollideWithBall(IBall ball, IBall otherBall)
+        private double CalculateDistance(double dx, double dy)
+        {
+            return Math.Sqrt(dx * dx + dy * dy);
+        }
+
+        private bool DoBallsCollide(IBall ball, IBall otherBall)
         {
             return CalculateDistance(ball.Position, otherBall.Position) < ball.Radius + otherBall.Radius;
         }
 
         private void BounceBalls(IBall ball1, IBall ball2)
         {
-            double sumOfWeights = ball1.Weight + ball2.Weight;
-            IVelocity velocity1After = AddVelocities(
-                MultiplyVelocity(ball1.Velocity, (ball1.Weight - ball2.Weight) / sumOfWeights),
-                MultiplyVelocity(ball2.Velocity, (2 * ball2.Weight) / sumOfWeights)
-                );
-            IVelocity velocity2After = AddVelocities(
-                MultiplyVelocity(ball1.Velocity, (2 * ball1.Weight) / sumOfWeights),
-                MultiplyVelocity(ball2.Velocity, (ball2.Weight - ball1.Weight) / sumOfWeights)
-                );
+            double dx = ball1.Position.X - ball2.Position.X;
+            double dy = ball1.Position.Y - ball2.Position.Y;
+            double distance = CalculateDistance(dx, dy);
 
-            ball1.Velocity = velocity1After;
-            ball2.Velocity = velocity2After;
+            if (distance == 0)
+            {
+                return;
+            }
+
+            double cosdx = dx / distance;
+            double cosdy = dy / distance;
+
+            if (!AreMovingTowardsEachOther(ball1, ball2, cosdx, cosdy))
+            {
+                return;
+            }
+
+            double v1Before = ball1.Velocity.X * cosdx + ball1.Velocity.Y * cosdy;
+            double v2Before = ball2.Velocity.X * cosdx + ball2.Velocity.Y * cosdy;
+            (double v1After, double v2After) = CalculateVelocitiesAfterCollision(v1Before, v2Before,
+                ball1.Weight, ball2.Weight);
+
+            ball1.Velocity.X += (v1After - v1Before) * cosdx;
+            ball1.Velocity.Y += (v1After - v1Before) * cosdy;
+            ball2.Velocity.X += (v2After - v2Before) * cosdx;
+            ball2.Velocity.Y += (v2After - v2Before) * cosdy;
         }
 
-        private IVelocity MultiplyVelocity(IVelocity velocity, double scalar)
+        private bool AreMovingTowardsEachOther(IBall ball1, IBall ball2, double cosdx, double cosdy)
         {
-            return new Velocity(velocity.X * scalar, velocity.Y * scalar);
+            double dvx = ball1.Velocity.X - ball2.Velocity.X;
+            double dvy = ball1.Velocity.Y - ball2.Velocity.Y;
+            double dot = CalculateDotProduct(dvx, dvy, cosdx, cosdy);
+            return dot < 0;
         }
 
-        private IVelocity AddVelocities(IVelocity velocity1, IVelocity velocity2)
+        private double CalculateDotProduct(double dx, double dy, double cosx, double cosy)
         {
-            return new Velocity(velocity1.X + velocity2.X, velocity1.Y + velocity2.Y);
+            return dx * cosx + dy * cosy;
         }
 
+        private (double, double) CalculateVelocitiesAfterCollision(double v1Before, double v2Before, double m1, double m2)
+        {
+            double v1After = (v1Before * (m1 - m2) + 2 * m2 * v2Before) / (m1 + m2);
+            double v2After = (v2Before * (m2 - m1) + 2 * m1 * v1Before) / (m1 + m2);
+            return (v1After, v2After);
+        }
 
-
-        private void HandleCollisionWithWall(IBall ball, IBoard board)
+        private void HandleCollisionWithWallAndMove(IBall ball, IBoard board)
         {
             if (DoesCollideWithHorizontalWalls(ball, board))
             {
@@ -183,10 +209,32 @@ namespace Logic
             double newX = ball.Position.X + ball.Velocity.X;
             return newX - ball.Radius < 0 || newX + ball.Radius > board.Width;
         }
+
         private bool DoesCollideWithVerticalWalls(IBall ball, IBoard board)
         {
             double newY = ball.Position.Y + ball.Velocity.Y;
             return newY - ball.Radius < 0 || newY + ball.Radius > board.Height;
+        }
+
+        private void MakeSureThatBallIsInsideBoard(IBall ball, IBoard board)
+        {
+            if (ball.Position.X - ball.Radius < 0)
+            {
+                ball.Position.X = ball.Radius;
+            }
+            else if (ball.Position.X + ball.Radius > board.Width)
+            {
+                ball.Position.X = board.Width - ball.Radius;
+            }
+
+            if (ball.Position.Y - ball.Radius < 0)
+            {
+                ball.Position.Y = ball.Radius;
+            }
+            else if (ball.Position.Y + ball.Radius > board.Height)
+            {
+                ball.Position.Y = board.Height - ball.Radius;
+            }
         }
     }
 }
